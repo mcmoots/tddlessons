dd21aa19648d5309dc0b5919a44e9715ca4c9a6d
mcmoots <criminy.crickets@gmail.com>
2015-03-11 16:42:19 -0700

    Chapter 4, refactor home page to use template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -15,7 +15,5 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        # NB: b'string' converts raw byte response to python string
-        self.assertIn(b'<title>List of Sea Creatures</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
