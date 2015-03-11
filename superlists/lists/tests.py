44f35f01f078848d9faec679961d059bc9473506
mcmoots <criminy.crickets@gmail.com>
2015-03-11 15:54:22 -0700

    Chapter 1, commit 4. URL mapping to dummy view.

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,8 +1,13 @@
+
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-# Create your tests here.
 
-class SillyTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad_math(self):
-        self.assertEqual(2+2, 5)
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
+
