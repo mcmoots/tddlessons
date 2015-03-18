6634626067be71f2e8d38cf0512dbc41f11dd629
mcmoots <criminy.crickets@gmail.com>
2015-03-17 18:37:37 -0700

    Ch6, rewriting functional tests for 2 users

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,4 +1,5 @@
 # Functional tests for TDD tutorial
+import unittest
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -22,6 +23,7 @@ class NewVisitorTest(LiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(self.live_server_url)
         
+        # Our first user is Myrtle. Myrtle visits the home page.
         # Page should announce itself as an app that makes lists of sea creatures
         self.assertIn('Sea Creatures', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
@@ -34,15 +36,17 @@ class NewVisitorTest(LiveServerTestCase):
             'Enter a sea creature'
         )
 
-        # User types "vampire squid" into a text box and hits enter
+        # Myrtle types "vampire squid" into a text box and hits enter
         inputbox.send_keys('vampire squid')
         inputbox.send_keys(Keys.ENTER)
 
-        # The page should update so that
-        # "1: Vampire squid" is an item in the sea creature list
-        self.check_for_item_in_list('1: vampire squid')
+        # Myrtle is taken to a new URL, which lists "1: vampire squid"
+        # as an item in a list
+        myrtle_list_url = self.browser.current_url
+        self.assertRegex(myrtle_list_url, '/lists/.+')
+        self.check_for_row_in_list_table('1: vampire squid')
 
-        # There will still be a text box inviting her to add another item.
+        # There will still be a text box inviting Myrtle to add another item.
         # Enters "echinoderm", and page updates to show both sea creatures.
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('echinoderm')
@@ -51,10 +55,32 @@ class NewVisitorTest(LiveServerTestCase):
         self.check_for_item_in_list('1: vampire squid')
         self.check_for_item_in_list('2: echinoderm')
 
-        # Site generates a unique URL for this list
-        # with some explanatory text about it
+        # now a new user, Archibald, visits the home page
+        # he shouldn't see anything of Myrtle's
 
-        # Visiting the URL results in the list of sea creatures reappearing
+        ## use a new browser session
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_elemend_by_tag_name('body').text
+        self.assertNotIn('vampire squid', page_text)
+        self.assertNotIn('echinoderm', page_text)
+
+        # Archibald starts a new list by entering a new sea creature.
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('herring')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Archibald gets his own URL
+        archibald_list_url = self.browser.current_url
+        self.assertRegex(archibald_list_url, '/lists/.+')
+        self.assertNotEqual(archibald_list_url, myrtle_list_url)
+
+        # Still no trace of Myrtle's list.
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('vampire squid', page_text)
+        self.assertIn('herring', page_text)
 
         # todo: test should clean up after itself
         self.fail("This test isn't finished yet! Write moar test!")
