10247a84cc72154421d72ceb39253804687ebb3e
mcmoots <criminy.crickets@gmail.com>
2015-03-18 23:52:42 -0700

    Ch6, refactoring URL rules

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'lists.views.home_page', name='home'),
-    # regex groups get passed as arguments to view
-    url(r'^lists/([^/]+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^lists/new$', 'lists.views.new_list', name='new_list'),
-    url(r'^lists/([^/]+)/add_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^lists/', include('lists.urls')),
 )
