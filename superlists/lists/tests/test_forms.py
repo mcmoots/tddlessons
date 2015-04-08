f957a8ed1f2ec0669b597bfa5b9d9d63b2b20c76
mcmoots <criminy.crickets@gmail.com>
2015-04-07 17:48:06 -0700

    Use new form in home_page and simplify test. NB: Breaks FTs

--- superlists/lists/tests/test_forms.py
+++ superlists/lists/tests/test_forms.py
@@ -3,6 +3,12 @@ from lists.forms import EMPTY_ITEM_ERROR, ItemForm
 
 class ItemFormTest(TestCase):
 
+    def test_form_input_has_placeholder_and_css_classes(self):
+        form = ItemForm()
+        self.assertIn('placeholder="Enter a sea creature"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
+
+
     def test_form_renders_item_text_input(self):
         form = ItemForm()
         self.assertIn('placeholder="Enter a sea creature"', form.as_p())
